d972b87f25c0e00f87be4a5d625b6072145aba60
gokul <gokulchandan24@gmail.com>
2024-12-14 01:01:48 +0530

    app setup done

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Container from "./components/container/Container";
+
+export default async function Home() {
+  // const user = await getCurrentUser();
+  // console.log(user)
+  return (
+    <div className="px-5 max-w-[1280px] mx-auto">
+      <Navbar />
+      <hr />
+      <Container />
+    </div>
+  );
+}
